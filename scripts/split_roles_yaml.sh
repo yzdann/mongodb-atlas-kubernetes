b01fb6da0ce0e3fbd61c264f04e215aa0b72b1df
Fulton Byrne <fulton.byrne@commercetools.de>
2021-06-30 11:50:51 -0400

    fx: split_roles_yaml.sh for MacOS

--- scripts/split_roles_yaml.sh
+++ scripts/split_roles_yaml.sh
@@ -2,10 +2,24 @@
 
 set -eou pipefail
 
+
+SPLIT_COMMAND=gsplit
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SPLIT_COMMAND=gcsplit
+    which $SPLIT_COMMAND
+    if [[ $? == 1 ]]; then
+        echo "Please install gcsplit"
+        echo "brew install coreutils"
+        exit
+    fi
+fi
+
+
+
 # This is the script that allows to avoid the restrictions from the controller-gen tool that puts both Role and ClusterRole
 # to the same role.yaml file (and kustomize doesn't provide an easy way to use only a single resource from file as a base)
 # So we simply split the 'config/rbac/roles.yaml' file into two new files
-csplit config/rbac/role.yaml '/---/' '{*}' &> /dev/null
+$SPLIT_COMMAND config/rbac/role.yaml '/---/' '{*}' &> /dev/null
 rm xx00
 mv xx01 config/rbac/clusterwide/role.yaml
 mv xx02 config/rbac/namespaced/role.yaml
